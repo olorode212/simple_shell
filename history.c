3d9c112684f74d89d374e09d4c5d3a3e07beb44d
MargaretNannyonjo <nannyonjoleon@gmail.com>
2023-10-18 16:49:31 -0700

    simple shell project

--- history.c
+++ history.c
@@ -1,126 +1,143 @@
 #include "shell.h"
+
 /**
- * write_history_to_file - writes history to a file
+ * get_history_file - gets the history file
  * @info: parameter struct
+ *
+ * Return: allocated string containg history file
+ */
+
+char *get_history_file(info_t *info)
+{
+	char *buf, *dir;
+
+	dir = _getenv(info, "HOME=");
+	if (!dir)
+		return (NULL);
+	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
+	if (!buf)
+		return (NULL);
+	buf[0] = 0;
+	_strcpy(buf, dir);
+	_strcat(buf, "/");
+	_strcat(buf, HIST_FILE);
+	return (buf);
+}
+
+/**
+ * write_history - creates a file, or appends to an existing file
+ * @info: the parameter struct
  *
  * Return: 1 on success, else -1
  */
-int write_history_to_file(info_t *info)
+int write_history(info_t *info)
 {
-char *filename = get_history_file(info);
+	ssize_t fd;
+	char *filename = get_history_file(info);
+	list_t *node = NULL;
 
-if (!filename)
-{
-{return -1; }
-}
-int fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
-free(filename);
+	if (!filename)
+		return (-1);
 
-if (fd == -1)
-{
-{return -1; }
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	free(filename);
+	if (fd == -1)
+		return (-1);
+	for (node = info->history; node; node = node->next)
+	{
+		_putsfd(node->str, fd);
+		_putfd('\n', fd);
+	}
+	_putfd(BUF_FLUSH, fd);
+	close(fd);
+	return (1);
 }
-write_history_entries(info, fd);
-close(fd);
-{return 1; }
-}
-void write_history_entries(info_t *info, int fd)
-{
-list_t *node = info->history;
 
-while (node)
+/**
+ * read_history - reads history from file
+ * @info: the parameter struct
+ *
+ * Return: histcount on success, 0 otherwise
+ */
+int read_history(info_t *info)
 {
-_putsfd(node->str, fd);
-_putfd('\n', fd);
-node = node->next;
-}
-_putfd(BUF_FLUSH, fd);
-}
-int read_history_from_file(info_t *info)
-{
-char *filename = get_history_file(info);
+	int i, last = 0, linecount = 0;
+	ssize_t fd, rdlen, fsize = 0;
+	struct stat st;
+	char *buf = NULL, *filename = get_history_file(info);
 
-if (!filename)
-{
-{return 0; }
-}
-int fd = open(filename, O_RDONLY);
-free(filename);
+	if (!filename)
+		return (0);
 
-if (fd == -1)
-{
-{return 0; }
+	fd = open(filename, O_RDONLY);
+	free(filename);
+	if (fd == -1)
+		return (0);
+	if (!fstat(fd, &st))
+		fsize = st.st_size;
+	if (fsize < 2)
+		return (0);
+	buf = malloc(sizeof(char) * (fsize + 1));
+	if (!buf)
+		return (0);
+	rdlen = read(fd, buf, fsize);
+	buf[fsize] = 0;
+	if (rdlen <= 0)
+		return (free(buf), 0);
+	close(fd);
+	for (i = 0; i < fsize; i++)
+		if (buf[i] == '\n')
+		{
+			buf[i] = 0;
+			build_history_list(info, buf + last, linecount++);
+			last = i + 1;
+		}
+	if (last != i)
+		build_history_list(info, buf + last, linecount++);
+	free(buf);
+	info->histcount = linecount;
+	while (info->histcount-- >= HIST_MAX)
+		delete_node_at_index(&(info->history), 0);
+	renumber_history(info);
+	return (info->histcount);
 }
-struct stat st;
-if (fstat(fd, &st) == -1 || st.st_size < 2)
-{
-close(fd);
-{return 0; }
-}
-char *buf = read_history_content(fd, st.st_size);
-close(fd);
 
-if (!buf)
+/**
+ * build_history_list - adds entry to a history linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ * @buf: buffer
+ * @linecount: the history linecount, histcount
+ *
+ * Return: Always 0
+ */
+int build_history_list(info_t *info, char *buf, int linecount)
 {
-{return 0; }
-}
-process_and_store_history(info, buf, st.st_size);
-free(buf);
+	list_t *node = NULL;
 
-{return renumber_history(info); }
-}
-char *read_history_content(int fd, off_t size)
-{
-char *buf = malloc(size + 1);
+	if (info->history)
+		node = info->history;
+	add_node_end(&node, buf, linecount);
 
-if (!buf)
-{
-{return NULL; }
+	if (!info->history)
+		info->history = node;
+	return (0);
 }
-ssize_t rdlen = read(fd, buf, size);
 
-if (rdlen <= 0)
+/**
+ * renumber_history - renumbers the history linked list after changes
+ * @info: Structure containing potential arguments. Used to maintain
+ *
+ * Return: the new histcount
+ */
+int renumber_history(info_t *info)
 {
-free(buf);
-{return NULL; }
-}
-buf[size] = '\0';
-{return buf; }
-}
-void process_and_store_history(info_t *info, char *buf, int size)
-{
-int last = 0;
-int linecount = 0;
+	list_t *node = info->history;
+	int i = 0;
 
-for (int i = 0; i < size; i++)
-{
-if (buf[i] == '\n')
-{
-buf[i] = '\0';
-add_history_entry(info, buf + last, &linecount);
-last = i + 1;
-}
-}
-info->histcount = linecount;
-
-while (info->histcount-- >= HIST_MAX)
-{
-delete_node_at_index(&(info->history), 0);
-}
-}
-void add_history_entry(info_t *info, char *entry, int *linecount)
-{
-if (info->history)
-{
-list_t *node = info->history;
-while (node->next)
-{
-node = node->next;
-}
-node->next = create_node(entry, (*linecount)++);
-}
-else
-}
-info->history = create_node(entry, (*linecount)++);
-}
+	while (node)
+	{
+		node->num = i++;
+		node = node->next;
+	}
+	return (info->histcount = i);
 }
